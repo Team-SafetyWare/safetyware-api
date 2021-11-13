72436368f73c7355d4c5fe624aa4f712cf37c4a9
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-11-13 07:57:20 -0700

    Add warp

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::Ipv4Addr;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run((Ipv4Addr::UNSPECIFIED, 3001))
+        .await;
 }
