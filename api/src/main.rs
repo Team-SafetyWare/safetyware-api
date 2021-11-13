1fc0f12b50c5d61bb2d9c86243500cfc98b5b155
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-11-13 08:10:21 -0700

    Add format configuration

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,10 +3,7 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
-    warp::serve(hello)
-        .run((Ipv4Addr::UNSPECIFIED, 3001))
-        .await;
+    warp::serve(hello).run((Ipv4Addr::UNSPECIFIED, 3001)).await;
 }
