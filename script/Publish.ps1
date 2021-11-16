ba697399cb5b86553821ba40aa88b21eb1a78590
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-11-16 00:34:20 -0700

    Show elapsed deploy time

--- script/Publish.ps1
+++ script/Publish.ps1
@@ -25,10 +25,16 @@ $env_hash = Get-StringHash -Plain "$org-$app-$env_name" -Length 13
 
 $rg_name = "rg-$app-$env_name"
 
+$start_time = $(get-date)
+
 Build-ApiFunc
 Publish-Database -App $app -EnvName $env_name
 Publish-AzureResourceGroup -Name $rg_name
 Publish-AzureTemplate -ResourceGroup $rg_name -EnvHash $env_hash
 Publish-ApiFunc -EnvHash $env_hash
+
+$elapsed_time = $(get-date) - $start_time
+
+Write-Host "Publish finished in $($elapsed_time.TotalSeconds) s." 
 
 Pop-Location
