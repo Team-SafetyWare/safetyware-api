c45a44b486292692f1f94eb98518df91d501465b
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-11-15 22:19:30 -0700

    Add publish script parameters

--- script/Publish.ps1
+++ script/Publish.ps1
@@ -1,12 +1,26 @@
+param(
+    [Parameter(Mandatory=$True)]
+    [System.String]
+    $Org,
+
+    [Parameter(Mandatory=$True)]
+    [System.String]
+    $App,
+
+    [Parameter(Mandatory=$True)]
+    [System.String]
+    $Env
+)
+
 Set-StrictMode -Version 3
 $ErrorActionPreference = "Stop"
 Push-Location $PSScriptRoot
 
 Import-Module .\Util.psm1 -Force
 
-$org = "cap"
-$app = "sw"
-$env_name = "dev"
+$org = $Org
+$app = $App
+$env_name = $Env
 $env_hash = Get-StringHash -Plain "$org-$app-$env_name" -Length 13
 
 $rg_name = "rg-$app-$env_name"
