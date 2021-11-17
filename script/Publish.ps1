e365e7ae753f375391650c221dc713f8e1923195
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-11-16 17:09:26 -0700

    Revert "Revert "SAF-39: Add cloud database connectivity""
    This reverts commit 49c117e88fe1e4d99eb2b6764c368d199e48f7b0.

--- script/Publish.ps1
+++ script/Publish.ps1
@@ -1,13 +1,13 @@
 param(
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory = $True)]
     [System.String]
     $Org,
 
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory = $True)]
     [System.String]
     $App,
 
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory = $True)]
     [System.String]
     $Env
 )
@@ -26,6 +26,21 @@ $env_hash = Get-StringHash -Plain "$org-$app-$env_name" -Length 13
 $rg_name = "rg-$app-$env_name"
 
 $start_time = $(get-date)
+
+# Todo: Do not delete and re-create the database user on every deploy. Part of SAF-41.
+$atlas_project = Get-AtlasProject -Name "$app-$env_name"
+$atlas_db_username = "app-api"
+$atlas_db_password = New-RandomPassword -Length 32
+if ( $null -ne (Get-AtlasDatabaseUser -ProjectId $atlas_project.id -Username $atlas_db_username) ) {
+    Remove-AtlasDatabaseUser -ProjectId $atlas_project.id -Username $atlas_db_username
+}
+Add-AtlasDatabaseUser -ProjectId $atlas_project.id -Username $atlas_db_username -Password $atlas_db_password
+New-AtlasCidrWhitelist "0.0.0.0/0" -ProjectId $atlas_project.id
+$atlas_db_uri_no_cred = Get-AtlasDatabaseUri -Cluster "db" -ProjectId $atlas_project.id
+$atlas_db_uri = ConvertTo-DatabaseUriWithCredentials `
+    -SrvUri $atlas_db_uri_no_cred.standardSrv `
+    -Username $atlas_db_username `
+    -Password $atlas_db_password
 
 Build-ApiFunc
 Publish-Database -App $app -EnvName $env_name
