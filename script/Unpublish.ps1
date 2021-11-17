7264dfa873ad5279933fc094f977807b4aa8471f
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-11-16 23:13:28 -0700

    Add incomplete unpublish script

---
+++ script/Unpublish.ps1
@@ -0,0 +1,36 @@
+param(
+    [Parameter(Mandatory = $True)]
+    [System.String]
+    $Org,
+
+    [Parameter(Mandatory = $True)]
+    [System.String]
+    $App,
+
+    [Parameter(Mandatory = $True)]
+    [System.String]
+    $Env
+)
+
+Set-StrictMode -Version 3
+$ErrorActionPreference = "Stop"
+Push-Location $PSScriptRoot
+
+Import-Module .\Util.psm1 -Force
+
+$org = $Org
+$app = $App
+$env_name = $Env
+$env_hash = Get-StringHash -Plain "$org-$app-$env_name" -Length 13
+
+$rg_name = "rg-$app-$env_name"
+
+$start_time = $(get-date)
+
+# Todo: Unpublish.
+
+$elapsed_time = $(get-date) - $start_time
+
+Write-Host "Unpublish finished in $($elapsed_time.TotalSeconds) s." 
+
+Pop-Location
