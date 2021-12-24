57b8ddbc1dfafcc7913eaa6412884ed7f5fda823
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-12-23 21:32:45 -0700

    Add tests

--- api/src/repo/mod.rs
+++ api/src/repo/mod.rs
@@ -1,13 +1,13 @@
-
 use futures_util::Stream;
-
-
-
-
 
 pub mod company;
 mod mongo_common;
 
-pub trait ItemStream<T>: Stream<Item = anyhow::Result<T>> {}
+pub trait ItemStream<T: Unpin>: Stream<Item = anyhow::Result<T>> + Unpin {}
 
-impl<T, I> ItemStream<I> for T where T: Stream<Item = anyhow::Result<I>> {}
+impl<T, I> ItemStream<I> for T
+where
+    T: Stream<Item = anyhow::Result<I>> + Unpin,
+    I: Unpin,
+{
+}
