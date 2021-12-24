57abcd41777a4b76f48542a4347580be9e02832e
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-12-23 20:47:13 -0700

    Delete database after test

--- api/tests/repo/company.rs
+++ api/tests/repo/company.rs
@@ -1,26 +1,34 @@
 use crate::repo;
 use api::repo::company::{Company, CompanyRepo, MongoCompanyRepo};
+use std::future::Future;
 use uuid::Uuid;
 
 #[tokio::test]
 async fn test_insert_one() {
-    let repo = repo().await.unwrap();
-    let company = Company {
-        id: Default::default(),
-        name: Uuid::new_v4().to_string(),
-    };
+    test_repo(|repo| async move {
+        let company = Company {
+            id: Default::default(),
+            name: Uuid::new_v4().to_string(),
+        };
 
-    repo.insert_one(&company).await.unwrap();
+        repo.insert_one(&company).await.unwrap();
 
-    let found = repo.find_one(company.id).await.unwrap();
-    assert!(found.is_some());
-    let found = found.unwrap();
-    assert_eq!(found.id, company.id);
-    assert_eq!(found.name, company.name);
+        let found = repo.find_one(company.id).await.unwrap();
+        assert!(found.is_some());
+        let found = found.unwrap();
+        assert_eq!(found.id, company.id);
+        assert_eq!(found.name, company.name);
+    })
+    .await
 }
 
-async fn repo() -> anyhow::Result<impl CompanyRepo> {
-    let db = repo::new_db().await?;
-    let repo = MongoCompanyRepo::new(db);
-    Ok(repo)
+async fn test_repo<T, F>(test: T)
+where
+    T: Fn(MongoCompanyRepo) -> F,
+    F: Future,
+{
+    let db = repo::new_db().await.unwrap();
+    let repo = MongoCompanyRepo::new(db.clone());
+    test(repo).await;
+    db.drop(None).await.unwrap();
 }
