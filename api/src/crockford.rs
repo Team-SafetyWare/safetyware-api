ffff2289165ab08900021631f015bf716f5d84e3
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-12-26 20:16:17 -0700

    Revert "Add unit test"
    This reverts commit b183816bf1826559c6cd9e2ba17c55b4871ff885.

--- api/src/crockford.rs
+++ api/src/crockford.rs
@@ -5,26 +5,3 @@ pub fn random_id() -> String {
     let bytes: [u8; 15] = rand::random();
     base32::encode(Alphabet::Crockford, &bytes).to_lowercase()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_random_id_len() {
-        // Act.
-        let id = random_id();
-
-        // Assert.
-        assert_eq!(id.len(), 24);
-    }
-
-    #[test]
-    fn test_random_id_unique() {
-        // Act.
-        let (a, b) = (random_id(), random_id());
-
-        // Assert.
-        assert_ne!(a, b);
-    }
-}
