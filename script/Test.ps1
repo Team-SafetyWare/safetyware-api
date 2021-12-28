1aca078311608c7aae153f086226f22a570061cb
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-12-27 17:22:18 -0700

    Write test pass or fail message

--- script/Test.ps1
+++ script/Test.ps1
@@ -8,26 +8,34 @@ Import-Module .\Util.psm1 -Force
 
 Push-Location $(Get-ProjectLocation)
 
-cargo build
-Confirm-LastExitCode
+try {
+    cargo build
+    Confirm-LastExitCode
+    
+    Start-Container mongo
+    
+    cargo test
+    Confirm-LastExitCode
+    
+    $env:SW_DB_URI="mongodb://localhost:42781"
+    $env:RUST_LOG="info"
+    $env:RUST_BACKTRACE="1"
+    $server_job = Start-Job -ScriptBlock { cargo run }
+    
+    docker run --rm --network host --volume $pwd/postman:/etc/newman -t postman/newman:5.3-alpine `
+        run API.postman_collection.json --global-var "base=http://host.docker.internal:3001"
+    
+    # Stop server before confirming Newman results.
+    Stop-Job $server_job.Id
+    Confirm-LastExitCode
 
-Start-Container mongo
+    Write-Host -ForegroundColor Green "Tests passed."
+}
+catch [Exception] {
+    Write-Output $_.Exception.GetType().FullName, $_.Exception.Message
+    Write-Host -ForegroundColor Red "Tests failed."
+}
 
-cargo test
-Confirm-LastExitCode
-
-$env:SW_DB_URI="mongodb://localhost:42781"
-$env:RUST_LOG="info"
-$env:RUST_BACKTRACE="1"
-$server_job = Start-Job -ScriptBlock { cargo run }
-
-docker run --rm --network host --volume $pwd/postman:/etc/newman -t postman/newman:5.3-alpine `
-    run API.postman_collection.json --global-var "base=http://host.docker.internal:3001"
-
-# Stop server before confirming Newman results.
-Stop-Job $server_job.Id
-Receive-Job -Keep $server_job.Id
-Confirm-LastExitCode
 
 Pop-Location
 
