8e002077d73ef28b11839782f2cf6261540ff872
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-12-27 17:29:15 -0700

    Fix lints

--- script/Test.ps1
+++ script/Test.ps1
@@ -1,3 +1,4 @@
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '')]
 param()
 
 Set-StrictMode -Version 3
@@ -11,20 +12,20 @@ Push-Location $(Get-ProjectLocation)
 try {
     cargo build
     Confirm-LastExitCode
-    
+
     Start-Container mongo
-    
+
     cargo test
     Confirm-LastExitCode
-    
+
     $env:SW_DB_URI="mongodb://localhost:42781"
     $env:RUST_LOG="info"
     $env:RUST_BACKTRACE="1"
     $server_job = Start-Job -ScriptBlock { cargo run }
-    
+
     docker run --rm --network host --volume $pwd/postman:/etc/newman -t postman/newman:5.3-alpine `
         run API.postman_collection.json --global-var "base=http://host.docker.internal:3001"
-    
+
     # Stop server before confirming Newman results.
     Stop-Job $server_job.Id
     Confirm-LastExitCode
