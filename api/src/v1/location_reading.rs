274c09b25c61a74385f9d93b9bcd1c1162c6bcb2
Owen Troke-Billard <owen.trokebillard@outlook.com>
2021-12-28 20:15:44 -0700

    Revert "Format"
    This reverts commit 46946f8105ea72c764512b4ff135d614f44a71c3.

--- api/src/v1/location_reading.rs
+++ api/src/v1/location_reading.rs
@@ -1,13 +1,17 @@
 use crate::repo::location_reading::LocationReading as RepoLocationReading;
 use crate::v1::{op, ResourceApi};
 
+
+
 use std::sync::Arc;
+
+
 
 use serde::{Deserialize, Serialize};
 
 use crate::repo::location_reading::LocationReadingRepo;
 use warp::filters::BoxedFilter;
-use warp::Reply;
+use warp::{Reply};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct LocationReading {
