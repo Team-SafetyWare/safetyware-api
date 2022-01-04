63bba1422b4f168a119da666388eec33fba7ad81
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-01-04 15:02:48 -0700

    Add GraphQL test utility

---
+++ api/tests/graphql/company.rs
@@ -0,0 +1,14 @@
+use crate::graphql::test_graphql;
+
+#[tokio::test]
+async fn test_get_company() {
+    test_graphql(|filter, context| async move {
+        // Arrange.
+
+        // Act.
+
+        // Assert.
+        assert!(false)
+    })
+    .await
+}
