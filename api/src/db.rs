35b4ed35af6cdf06958baddbe39b6dab7a869703
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-02-13 16:48:49 -0700

    Add team person index

--- api/src/db.rs
+++ api/src/db.rs
@@ -1,6 +1,6 @@
 use crate::crockford;
 use bson::Document;
-use mongodb::{Client, Database, IndexModel};
+use mongodb::{Client, Collection, Database, IndexModel};
 
 pub const DB_NAME: &str = "sw";
 
@@ -43,84 +43,65 @@ pub async fn prepare(db: &Database) -> anyhow::Result<()> {
     prepare_coll_incident(db).await?;
     prepare_coll_location_reading(db).await?;
     prepare_coll_person(db).await?;
+    prepare_coll_team(db).await?;
     Ok(())
 }
 
 pub async fn prepare_coll_gas_reading(db: &Database) -> anyhow::Result<()> {
-    let collection = db.collection::<Document>(coll::GAS_READING);
-    collection
-        .create_index(
-            IndexModel::builder()
-                .keys(bson::doc! { "person_id": 1 })
-                .build(),
-            None,
-        )
-        .await?;
-    collection
-        .create_index(
-            IndexModel::builder().keys(bson::doc! { "gas": 1 }).build(),
-            None,
-        )
-        .await?;
-    collection
-        .create_index(
-            IndexModel::builder()
-                .keys(bson::doc! { "location": "2dsphere" })
-                .build(),
-            None,
-        )
-        .await?;
+    let collection = db.collection(coll::GAS_READING);
+    create_simple_index(&collection, "person_id").await?;
+    create_simple_index(&collection, "gas").await?;
+    create_2dsphere_index(&collection, "location").await?;
     Ok(())
 }
 
 pub async fn prepare_coll_incident(db: &Database) -> anyhow::Result<()> {
-    let collection = db.collection::<Document>(coll::INCIDENT);
-    collection
-        .create_index(
-            IndexModel::builder()
-                .keys(bson::doc! { "person_id": 1 })
-                .build(),
-            None,
-        )
-        .await?;
-    collection
-        .create_index(
-            IndexModel::builder()
-                .keys(bson::doc! { "location": "2dsphere" })
-                .build(),
-            None,
-        )
-        .await?;
+    let collection = db.collection(coll::INCIDENT);
+    create_simple_index(&collection, "person_id").await?;
+    create_2dsphere_index(&collection, "location").await?;
     Ok(())
 }
 
 pub async fn prepare_coll_location_reading(db: &Database) -> anyhow::Result<()> {
-    let collection = db.collection::<Document>(coll::LOCATION_READING);
+    let collection = db.collection(coll::LOCATION_READING);
+    create_simple_index(&collection, "person_id").await?;
+    create_2dsphere_index(&collection, "location").await?;
+    Ok(())
+}
+
+pub async fn prepare_coll_person(db: &Database) -> anyhow::Result<()> {
+    let collection = db.collection(coll::PERSON);
+    create_simple_index(&collection, "company_id").await?;
+    Ok(())
+}
+
+pub async fn prepare_coll_team(db: &Database) -> anyhow::Result<()> {
+    let collection = db.collection(coll::TEAM);
+    create_simple_index(&collection, "people.person_id").await?;
+    Ok(())
+}
+
+pub async fn create_simple_index(
+    collection: &Collection<Document>,
+    field: &str,
+) -> anyhow::Result<()> {
     collection
         .create_index(
-            IndexModel::builder()
-                .keys(bson::doc! { "person_id": 1 })
-                .build(),
-            None,
-        )
-        .await?;
-    collection
-        .create_index(
-            IndexModel::builder()
-                .keys(bson::doc! { "location": "2dsphere" })
-                .build(),
+            IndexModel::builder().keys(bson::doc! { field: 1 }).build(),
             None,
         )
         .await?;
     Ok(())
 }
 
-pub async fn prepare_coll_person(db: &Database) -> anyhow::Result<()> {
-    let collection = db.collection::<Document>(coll::PERSON);
+pub async fn create_2dsphere_index(
+    collection: &Collection<Document>,
+    field: &str,
+) -> anyhow::Result<()> {
     collection
         .create_index(
             IndexModel::builder()
-                .keys(bson::doc! { "company_id": 1 })
+                .keys(bson::doc! { field: "2dsphere" })
                 .build(),
             None,
         )
