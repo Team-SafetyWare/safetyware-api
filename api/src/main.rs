c49e22c8f5bc14e5c8c27be883c8813d38e67550
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-02-24 20:50:16 -0700

    Put doc filter in its own method

--- api/src/main.rs
+++ api/src/main.rs
@@ -63,11 +63,15 @@ fn filter(
 ) -> BoxedFilter<(impl Reply,)> {
     graphql::graphql_filter(graphql_context)
         .or(graphql::graphiql_filter())
-        .or(warp::path("doc").and(warp::fs::dir("doc/public")))
+        .or(doc())
         .or(import::device_data_filter(import_device_data_context))
         .or(health(db))
         .or(robots())
         .boxed()
+}
+
+fn doc() -> BoxedFilter<(impl Reply,)> {
+    warp::path("doc").and(warp::fs::dir("doc/public")).boxed()
 }
 
 fn robots() -> BoxedFilter<(impl Reply,)> {
