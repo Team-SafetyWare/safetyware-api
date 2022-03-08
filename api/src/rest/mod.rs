369adfb89b206e89085b7765c5c521db3e0570a4
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-08 03:19:32 -0700

    Add default image

--- api/src/rest/mod.rs
+++ api/src/rest/mod.rs
@@ -1,11 +1,16 @@
+use crate::image::PngBytes;
 use crate::repo::user_account::ArcUserAccountRepo;
 use crate::warp_ext;
+use image::{DynamicImage, RgbImage};
 use mongodb::Database;
 use warp::filters::BoxedFilter;
 use warp::http::header::CONTENT_TYPE;
 use warp::http::HeaderValue;
 use warp::reply::Response;
 use warp::{Filter, Reply};
+
+const DEFAULT_PROFILE_IMAGE_SIZE: u32 = 512;
+const DEFAULT_PROFILE_IMAGE_SHADE: u8 = 128;
 
 #[derive(Clone)]
 pub struct Context {
@@ -35,11 +40,21 @@ fn user_account_profile_image(user_account_repo: ArcUserAccountRepo) -> BoxedFil
         .and(warp_ext::with_clone(user_account_repo))
         .then(
             move |user_account_id: String, user_account_repo: ArcUserAccountRepo| async move {
-                // Todo: Use default image if none.
+                let default_pixels = vec![
+                    DEFAULT_PROFILE_IMAGE_SHADE;
+                    DEFAULT_PROFILE_IMAGE_SIZE.pow(2) as usize * 3
+                ];
+                let default_buffer = RgbImage::from_raw(
+                    DEFAULT_PROFILE_IMAGE_SIZE,
+                    DEFAULT_PROFILE_IMAGE_SIZE,
+                    default_pixels,
+                )
+                .unwrap();
+                let default_png_bytes = DynamicImage::ImageRgb8(default_buffer).png_bytes()?;
                 let png_bytes = user_account_repo
                     .profile_image_png(&user_account_id)
                     .await?
-                    .expect("profile image missing");
+                    .unwrap_or(default_png_bytes);
                 let mut response = Response::new(png_bytes.into());
                 response
                     .headers_mut()
