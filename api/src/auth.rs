7810d857dd2b24e1b2f03fb51551dd6c24c847cc
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-23 04:38:55 -0600

    Make some values constant

--- api/src/auth.rs
+++ api/src/auth.rs
@@ -5,16 +5,17 @@ use ring::pbkdf2;
 use ring::rand::{SecureRandom, SystemRandom};
 use std::num::NonZeroU32;
 
+const N_ITER: u32 = 100_000;
+const CREDENTIAL_LEN: usize = SHA512_OUTPUT_LEN;
+
 fn create_creds(password: String) -> Creds {
-    const CREDENTIAL_LEN: usize = SHA512_OUTPUT_LEN;
-    let n_iter = NonZeroU32::new(100_000).unwrap();
     let rng = SystemRandom::new();
     let mut salt = [0u8; CREDENTIAL_LEN];
     rng.fill(&mut salt).unwrap();
     let mut pbkdf2_hash = [0u8; CREDENTIAL_LEN];
     pbkdf2::derive(
         pbkdf2::PBKDF2_HMAC_SHA512,
-        n_iter,
+        NonZeroU32::new(N_ITER).unwrap(),
         &salt,
         password.as_bytes(),
         &mut pbkdf2_hash,
@@ -26,7 +27,6 @@ fn create_creds(password: String) -> Creds {
 }
 
 fn verify_password(password: String, creds: Creds) -> Result<(), ()> {
-    let n_iter = NonZeroU32::new(100_000).unwrap();
     let password_hash = HEXLOWER_PERMISSIVE
         .decode(creds.password_hash.as_bytes())
         .map_err(|_| ())?;
@@ -35,7 +35,7 @@ fn verify_password(password: String, creds: Creds) -> Result<(), ()> {
         .map_err(|_| ())?;
     pbkdf2::verify(
         pbkdf2::PBKDF2_HMAC_SHA512,
-        n_iter,
+        NonZeroU32::new(N_ITER).unwrap(),
         &salt,
         password.as_bytes(),
         &password_hash,
